369a8f1606a82bc69f3ac3b15fa19db184d6a716
Naina Johri <jhorinaina8@gmail.com>
2024-07-29 12:26:57 +0530

    put and deleted api created for person with id

--- router/PersonRouter.js
+++ router/PersonRouter.js
@@ -19,7 +19,7 @@ router.post("/", async (req, res) => {
   }
 });
 
-// GET API to get person acc to its worktype
+// GET API to get person acc to its  worktype
 router.get("/:workType", async (req, res) => {
   try {
     const workType = req.params.workType;
@@ -45,6 +45,43 @@ router.get("/", async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const personId = req.params.id; //Extract the id from the URL parameter
+    const updatedPersonData = req.body; //Updated data for the person
+    const response = await Person.findByIdAndUpdate(
+      personId,
+      updatedPersonData,
+      {
+        new: true, //Return the updated document
+        runValidators: true, // Run Mongoose Validation
+      }
+    );
+    if (!response) {
+      return res.status(404).json({ error: "Person not found" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server Error" });
+  }
+});
+
+router.delete("/:id", async (res, req) => {
+  try {
+    const personId = req.params.id;
+
+    const response = await Person.findByIdAndRemove(personId);
+    if (!response) {
+      return res.status(404).json({ error: "Person not found" });
+    }
+    console.log("data deleted");
+    res.status(200).json({ message: "Person Deleted Successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server Error" });
   }
 });
 
