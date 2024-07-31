7506ed249a9b06501c96a00d915bfbc90e2db430
Naina Johri <jhorinaina8@gmail.com>
2024-07-31 21:50:36 +0530

    /signup post request created and jwt.js file created

--- router/PersonRouter.js
+++ router/PersonRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Person = require("../models/person");
 
-router.post("/", async (req, res) => {
+router.post("/signup", async (req, res) => {
   try {
     const data = req.body; //Assuming the request body contains the person data
 
